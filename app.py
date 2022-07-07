87a808c80a259dcb25bedac965515e8b8145df12
Mark Cruz <markaycruz@gmail.com>
2022-07-07 19:31:47 -0400

    changes

--- app.py
+++ app.py
@@ -2,39 +2,43 @@ import boto3
 import json
 
 def handler(event, context):
-    body = ""
-    client = boto3.resource('dynamodb')
-    table = client.Table('python-service-db')
-    
-    print(event)
-    
-    if event['routeKey'] == "GET /items":
-        res = table.get_item(
-            Key = {
-                'id': event['queryStringParameters']['id']
+    try:
+        body = ""
+        client = boto3.resource('dynamodb')
+        table = client.Table('python-service-db')
+        
+        print(event)
+        
+        if event['routeKey'] == "GET /items":
+            res = table.get_item(
+                Key = {
+                    'id': event['queryStringParameters']['id']
+                    }
+                )
+            body = res['Item']
+        elif event['routeKey'] == "POST /items":
+            describeTable = table.describe_table(TableName='python-service-db')
+            table.put_item(
+                Item = {
+                    'id': describeTable['Table']['ItemCount'],
+                    'firstName': event['queryStringParameters']['firstName'],
+                    'lastName': event['queryStringParameters']['lastName']
+                    }
+                )
+            body = {
+                'message': 'Successfully added!'
                 }
-            )
-        print(res)
-        body = res
-    elif event['routeKey'] == "POST /items":
-        describeTable = table.describe_table(TableName='python-service-db')
-        table.put_item(
-            Item = {
-                'id': describeTable['Table']['ItemCount'],
-                'firstName': event['queryStringParameters']['firstName'],
-                'lastName': event['queryStringParameters']['lastName']
-                }
-            )
-        body = {
-            'message': 'Successfully added!'
+        
+        if body == "":
+            return {
+                'statusCode': 400,
             }
-    
-    if body == "":
+        else:
+            return {
+                'statusCode': 200,
+                'body': body
+            }
+    except:
         return {
-            'statusCode': 400,
-        }
-    else:
-        return {
-            'statusCode': 200,
-            'body': body
-        }
+                'statusCode': 400,
+            }
