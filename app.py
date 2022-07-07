fd26b9c7e080619df533cb7082a65bd12e46c10e
Mark Cruz <markaycruz@gmail.com>
2022-07-07 01:33:51 -0400

    change return logic

--- app.py
+++ app.py
@@ -2,41 +2,39 @@ import boto3
 import json
 
 def handler(event, context):
-    try:
-        client = boto3.resource('dynamodb')
-        table = client.Table('python-service-db')
-        
-        print(event)
-        
-        if event['routeKey'] == "GET /items":
-            res = table.get_item(
-                Key = {
-                    'firstName': event['queryStringParameters']['firstName'],
-                    'lastName': event['queryStringParameters']['lastName']
-                    }
-                )
-            body = res
-        elif event['routeKey'] == "POST /items":
-            describeTable = table.describe_table(TableName='python-service-db')
-            table.put_item(
-                Item = {
-                    'id': describeTable['Table']['ItemCount'],
-                    'firstName': event['queryStringParameters']['firstName'],
-                    'lastName': event['queryStringParameters']['lastName']
-                    }
-                )
-            body = {
-                'message': 'Successfully added!'
+    body = ""
+    client = boto3.resource('dynamodb')
+    table = client.Table('python-service-db')
+    
+    print(event)
+    
+    if event['routeKey'] == "GET /items":
+        res = table.get_item(
+            Key = {
+                'firstName': event['queryStringParameters']['firstName'],
+                'lastName': event['queryStringParameters']['lastName']
                 }
+            )
+        body = res
+    elif event['routeKey'] == "POST /items":
+        describeTable = table.describe_table(TableName='python-service-db')
+        table.put_item(
+            Item = {
+                'id': describeTable['Table']['ItemCount'],
+                'firstName': event['queryStringParameters']['firstName'],
+                'lastName': event['queryStringParameters']['lastName']
+                }
+            )
+        body = {
+            'message': 'Successfully added!'
+            }
+    
+    if body == "":
+        return {
+            'statusCode': 400,
+        }
+    else:
         return {
             'statusCode': 200,
             'body': body
         }
-        
-    except Exception as e:
-        return {
-            'statusCode': 400,
-            'body': e
-        }
-        
-
