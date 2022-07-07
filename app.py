bb1c505eda0703d10e48b074b9cc825a173244fb
Mark Cruz <markaycruz@gmail.com>
2022-07-07 00:18:12 -0400

    changes

--- app.py
+++ app.py
@@ -1,3 +1,46 @@
-import sys
+import boto3
+import json
+
 def handler(event, context):
-    return 'Hello from AWS Lambda using Python' + sys.version + '!'        
+    
+    eventJson = json.loads(event)
+    
+    try:
+        client = boto3.resource('dynamodb')
+        table = client.Table('python-service-db')
+        
+        table.put_item(Item= {
+            'id': '0',
+            'name': 'Mark'
+        })
+        
+        # if eventJson['routeKey'] is "GET /items/{id}":
+        #     table.get_item({
+        #     "Key": {
+        #       'id': event.pathParameters.id
+        #         }
+        #     })
+        #     body = {
+        #         'message': 'Item added!'
+        #     }
+        # elif eventJson['routeKey'] is "POST /items":
+        #     table.put_item(
+                
+        #     )
+            
+        # return {
+        #     'statusCode': 200,
+        #     'body': body
+        # }
+        return {
+            'status': 200,
+            'message': 'Successfully added!'
+        }
+        
+    except Exception as e:
+        return {
+            'statusCode': 400,
+            'body': e['message']
+        }
+        
+
--- wsgi.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-wsgi.py: Wsgi init
-"""
-
-import api
-
-app = api.app
